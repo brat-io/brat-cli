4efce04422179c39b0217396af5ef04ec35aa9a8
John-Henry Liberty <john-henry.liberty@activenetwork.com>
2015-02-02 08:06:02 -0600

    Add namespace, api_version, and host to config for modularity

--- lib/brat/configuration.rb
+++ lib/brat/configuration.rb
@@ -2,7 +2,8 @@ module Brat
   # Defines constants and methods related to configuration.
   module Configuration
     # An array of valid keys in the options hash when configuring a Brat::API.
-    VALID_OPTIONS_KEYS = [:endpoint, :private_token, :user_agent, :sudo].freeze
+    VALID_OPTIONS_KEYS = [:endpoint, :private_token, :user_agent, :sudo,
+                           :namespace, :api_version, :host].freeze
 
     # The user agent that will be sent to the API endpoint if none is set.
     DEFAULT_USER_AGENT = "Brat Ruby Gem #{Brat::VERSION}".freeze
@@ -30,8 +31,8 @@ module Brat
 
     # Resets all configuration options to the defaults.
     def reset
-      self.endpoint       = ENV['GITLAB_API_ENDPOINT']
-      self.private_token  = ENV['GITLAB_API_PRIVATE_TOKEN']
+      self.endpoint       = ENV['BRAT_API_ENDPOINT']
+      self.private_token  = ENV['BRAT_API_PRIVATE_TOKEN']
       self.sudo           = nil
       self.user_agent     = DEFAULT_USER_AGENT
     end
